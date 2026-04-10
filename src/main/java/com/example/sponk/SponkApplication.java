e5710d9bdffc28e0f1ec49cad91a45dec702468d
Aleksei <morgachev_aleksey@mail.ru>
2026-04-11 01:22:44 +0300

    v.0 Создание проекта

---
+++ src/main/java/com/example/sponk/SponkApplication.java
@@ -0,0 +1,13 @@
+package com.example.sponk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SponkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SponkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sponk/SponkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sponk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SponkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
